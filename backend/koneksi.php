d2d62eb4266539e719fbad709f7d7198926d072a
sabrinasalsa <Bilasalsabila1716@gmail.com>
2026-06-18 11:31:30 +0800

    hapus debug

--- backend/koneksi.php
+++ backend/koneksi.php
@@ -1,12 +1,10 @@
 <?php
-
-
 $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
 $db_name = 'caredrop';
 
-// ── PDO connection ──────────────────────────────────────────────────────────
+//PDO connection
 try {
     $pdo = new PDO(
         "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4",
